cab248005b299c9701ad98fe9f9f0fe1490b0abb
Haniep Fathan Riziq <haniffathan33@gmail.com>
2023-11-13 17:01:45 +0700

    test

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const Login = (data, callback) => {
   axios
-    .post(`${process.env.SERVER_URL}/api/v1/login`, data, { withCredentials: true })
+    .post(`${import.meta.env.SERVER_URL}/api/v1/login`, data, { withCredentials: true })
     .then((res) => {
       callback(true, res);
     })
@@ -14,7 +14,7 @@ export const Login = (data, callback) => {
 
 export const Register = (data, callback) => {
   axios
-    .post(`${process.env.SERVER_URL_VERCEL}/api/v1/register`, data)
+    .post(`${import.meta.env.SERVER_URL_VERCEL}/api/v1/register`, data)
     .then((res) => {
       callback(true, res);
       console.log(true, res);
