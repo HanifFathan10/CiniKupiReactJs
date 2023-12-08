dd5e8b0bfddc33868aeaaf2d48727201650f1758
Haniep Fathan Riziq <haniffathan33@gmail.com>
2023-12-08 15:34:40 +0700

    refactor/hamburger menu

--- src/component/Elements/NavigasiBar/Navigasi.jsx
+++ src/component/Elements/NavigasiBar/Navigasi.jsx
@@ -7,13 +7,13 @@ const Navigasi = () => {
 
   useEffect(() => {
     const handleDocumentClick = (event) => {
-      if (event.target.closest('.toggle-button') === null) {
+      if (event.target.closest(".toggle-button") === null) {
         setOpen(false);
       }
     };
-    document.addEventListener('click', handleDocumentClick);
+    document.addEventListener("click", handleDocumentClick);
     return () => {
-      document.removeEventListener('click', handleDocumentClick);
+      document.removeEventListener("click", handleDocumentClick);
     };
   }, []);
 
@@ -23,21 +23,13 @@ const Navigasi = () => {
 
   return (
     <>
-      <div
-        onClick={handleToggleClick}
-        className="text-2xl absolute right-6 top-5 cursor-pointer md:hidden toggle-button"
-      >
-        <ion-icon name={open ? "close" : "menu"} />
+      <div onClick={handleToggleClick} className="text-2xl absolute right-5 top-5 cursor-pointer md:hidden flex flex-col toggle-button">
+        <span className={`w-6 h-[4px] bg-light my-[2px] transition-all duration-300 ${open && "origin-top-left rotate-45"}`}></span>
+        <span className={`w-6 h-[4px] bg-light my-[2px] transition-all duration-300 ${open && "scale-0"}`}></span>
+        <span className={`w-6 h-[4px] bg-light my-[2px] transition-all duration-300 ${open && "origin-bottom-left -rotate-45"}`}></span>
       </div>
-      <ul
-        className={`md:flex md:items-center bg-[#212121] max-w-[125px] text-sm md:text-md md:pb-0 pb-2 absolute md:max-w-full md:static md:z-auto right-4 w-full md:w-auto md:bg-transparent rounded-lg md:pl-0 pl-6 transition-all duration-700 ease-in-out ${
-          open ? "top-20 opacity-100" : "left-[700px] opacity-0 md:opacity-100"
-        }`}
-      >
-        <NavigasiList
-          classnameLi="md:ml-8 md:my-0 my-5"
-          classnameA="text-md max-md:text-[#ffffff] font-semibold px-2 hover:text-slate-600 duration-500"
-        />
+      <ul className={`md:flex md:items-center bg-[#212121] max-w-[125px] text-sm md:text-md md:pb-0 pb-2 absolute md:max-w-full md:static md:z-auto right-4 w-full md:w-auto md:bg-transparent rounded-lg md:pl-0 pl-6 transition-all duration-700 ease-in-out ${open ? "top-20 opacity-100" : "left-[700px] opacity-0 md:opacity-100"}`}>
+        <NavigasiList classnameLi="md:ml-8 md:my-0 my-5" classnameA="text-md max-md:text-[#ffffff] font-semibold px-2 hover:text-slate-600 duration-500" />
       </ul>
     </>
   );
